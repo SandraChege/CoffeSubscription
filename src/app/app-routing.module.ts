3993bf5f00e1545a7291a5ea4e73813eef85b611
Sandra Chege <chegemwihaki88@gmail.com>
2023-11-17 12:04:40 +0300

    Coffee subcription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingpageComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'nav', component: NavbarComponent },
+
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
